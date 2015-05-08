631c4077bcddcd5af6b566c2878c512d789908c2
Aarskin <mwiemer@outlook.com>
2015-05-07 22:39:33 -0500

    check

---
+++ app/src/main/java/com/example/matthew/constellate/VectorPair.java
@@ -0,0 +1,14 @@
+package com.example.matthew.constellate;
+
+/**
+ * Created by Matthew on 5/7/2015.
+ */
+public class VectorPair
+{
+    public Vector v1, v2;
+
+    public VectorPait(Vector3 a, Vector3 b)
+    {
+        
+    }
+}
